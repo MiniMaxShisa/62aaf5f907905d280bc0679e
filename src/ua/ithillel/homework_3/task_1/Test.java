319db8f48ad1c3a108040c3d75347739e558b10b
MiniMaxShisa <maxim.shisa@gmail.com>
2022-08-11 19:38:54 +0300

    deleted Test

--- src/ua/ithillel/homework_3/task_1/Test.java
+++
@@ -1,8 +0,0 @@
-package ua.ithillel.homework_3.task_1;
-
-public class Test {
-    public static void main(String[] args) {
-        Employee employee = new Employee("name", "job", "email@email", 111222333, 25);
-        System.out.println(employee);
-    }
-}
